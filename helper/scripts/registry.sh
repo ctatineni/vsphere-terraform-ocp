0de63d905aeb953d369da61b9a00973b636edf81
chandhubabu <chandhut@gmail.com>
2020-08-25 14:38:32 -0500

    Update registry.sh

--- helper/scripts/registry.sh
+++ helper/scripts/registry.sh
@@ -14,7 +14,7 @@ curl -u openshift:password -k https://$host:5000/v2/_catalog
 sudo podman login -u openshift -p password --authfile $HOME/pullsecret_config.json $host:5000
 jq -c --argjson var "$(jq .auths $HOME/pullsecret_config.json)" '.auths += $var' $HOME/ocp_pullsecret.json > $HOME/merged_pullsecret.json
 
-export OCP_RELEASE=4.4.18-x86_64
+export OCP_RELEASE=4.5.7-x86_64
 export LOCAL_REGISTRY=$host:5000
 export LOCAL_REPOSITORY='ocp4/openshift4' 
 export PRODUCT_REPO='openshift-release-dev' 
